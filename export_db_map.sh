04bc3a08898fcdfc1dc0810830d3cf4f1f8c905d
Matthieu Haefele <matthieu@haefele.fr>
2021-08-31 10:22:59 +0200

    Putting database password in a dedicated local file

--- export_db_map.sh
+++ export_db_map.sh
@@ -1,11 +1,13 @@
 PUBLIC_HTML=/var/www/html
 MDDB_DIR=$HOME/scripts
 DATE=$(date +%Y-%m-%d)
+source $MDDB_DIR/dolibarr_db_pass.sh
 
 mkdir -p $PUBLIC_HTML/$DATE
 cd $PUBLIC_HTML/$DATE
 python3 $MDDB_DIR/manage_dolibarr_db.py export -f json > $MDDB_DIR/logs/$DATE.log 2>&1
 python3 $MDDB_DIR/manage_dolibarr_db.py export -f csv 
+python3 $MDDB_DIR/manage_dolibarr_db.py update --dry_run >> $MDDB_DIR/logs/$DATE.log 2>&1
 cd ..
 ls
 rm current
